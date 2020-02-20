0d775ed1a35932aa8fcdc3f34ab2958a605209da
Gabriel M. Fernandes <mfgabriel92@gmail.com>
2020-02-20 14:41:50 -0300

    Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'section-xiii-observables';
+export class AppComponent implements OnInit {
+  isActive = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.activated.subscribe((isActivated: boolean) => {
+      this.isActive = isActivated;
+    });
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   id: number;
+  activation: Subscription;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
+    this.activation = this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.activation.unsubscribe();
+  }
+
+  onActivateClick(): void {
+    this.userService.activated.next(true);
   }
 }
